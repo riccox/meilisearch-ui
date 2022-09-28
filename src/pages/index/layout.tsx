3f2eadb7ceb3b992af743789ab1eed01ff11ab05
lrvinye <lrvinye@qq.com>
2022-09-28 22:16:51 +0800

    feat: use echart for index FieldDistribution display

--- src/pages/index/layout.tsx
+++ src/pages/index/layout.tsx
@@ -1,13 +1,22 @@
 import { Header } from '@/src/components/Header';
-import { MouseEventHandler, useCallback, useMemo } from 'react';
-import { ActionIcon, Badge, Button, Tooltip } from '@mantine/core';
+import { MouseEventHandler, useCallback, useMemo, useState } from 'react';
+import { ActionIcon, Badge, Button, Modal, Tooltip } from '@mantine/core';
 import { useIndexes } from '@/src/hooks/useIndexes';
 import { useInstanceStats } from '@/src/hooks/useInstanceStats';
 import { Link, Outlet, useNavigate, useSearchParams } from 'react-router-dom';
 import { Index } from 'meilisearch';
 import { useMeiliClient } from '@/src/hooks/useMeiliClient';
-import { IconAdjustments, IconSquarePlus } from '@tabler/icons';
+import { IconAbacus, IconAdjustments, IconSquarePlus } from '@tabler/icons';
 import qs from 'qs';
+
+import ReactECharts from 'echarts-for-react'; // Import the echarts core module, which provides the necessary interfaces for using echarts.
+import * as echarts from 'echarts/core'; // Import charts, all with Chart suffix
+import { BarChart } from 'echarts/charts'; // import components, all suffixed with Component
+import { GridComponent, TitleComponent, TooltipComponent } from 'echarts/components'; // Import renderer, note that introducing the CanvasRenderer or SVGRenderer is a required step
+import { CanvasRenderer } from 'echarts/renderers'; // Register the required components
+import _ from 'lodash';
+// Register the required components
+echarts.use([TitleComponent, TooltipComponent, GridComponent, BarChart, CanvasRenderer]);
 
 function IndexesLayout() {
   const navigate = useNavigate();
@@ -15,6 +24,8 @@ function IndexesLayout() {
   const stats = useInstanceStats(client);
   const [indexes, indexesQuery] = useIndexes(client);
   const [searchParams] = useSearchParams();
+  const [isFieldDistributionDetailModalOpen, setIsFieldDistributionDetailModalOpen] = useState(false);
+  const [fieldDistributionDetailChartIndex, setFieldDistributionDetailChartIndex] = useState<Index>(indexes[0]);
 
   const onClickIndex = useCallback(
     (index: Index, to: string) => {
@@ -22,6 +33,108 @@ function IndexesLayout() {
     },
     [navigate]
   );
+
+  const onClickFieldDistribution = useCallback((index: Index) => {
+    if (index) {
+      setIsFieldDistributionDetailModalOpen(true);
+      setFieldDistributionDetailChartIndex(index);
+    }
+  }, []);
+
+  const fieldDistributionChartOpt = useMemo(() => {
+    const source = _.toPairs(stats?.indexes[fieldDistributionDetailChartIndex?.uid]?.fieldDistribution);
+    console.debug('fieldDistributionChartOpt', stats?.indexes, fieldDistributionDetailChartIndex?.uid, source);
+    const largest = source.sort((a, b) => b[1] - a[1])[0] ?? 0;
+    return {
+      title: {
+        show: true,
+        text: 'Field Distribution',
+        subtext: 'Go to official docs about Field Distribution',
+        sublink: 'https://docs.meilisearch.com/reference/api/stats.html#stats-object',
+      },
+      tooltip: {
+        show: true,
+        type: 'axis',
+      },
+      dataset: [
+        {
+          dimensions: ['Field', 'Count'],
+          source,
+        },
+        {
+          transform: {
+            type: 'sort',
+            config: { dimension: 'Count', order: 'asc' },
+          },
+        },
+      ],
+      // make label show full width text
+      grid: { containLabel: true },
+      dataZoom: [
+        {
+          type: 'inside',
+          start: 0,
+          end: largest[1] + 100,
+        },
+        {
+          start: 0,
+          end: largest[1] + 100,
+        },
+      ],
+      yAxis: {
+        type: 'category',
+        axisLabel: {
+          color: '#909090',
+        },
+        axisTick: {
+          show: false,
+        },
+        axisLine: {
+          show: false,
+        },
+        z: 10,
+      },
+      xAxis: {
+        position: 'top',
+        axisLine: {
+          show: false,
+        },
+        axisTick: {
+          show: false,
+        },
+        axisLabel: {
+          color: '#999',
+        },
+      },
+      series: [
+        {
+          type: 'bar',
+          datasetIndex: 1,
+          showBackground: false,
+          itemStyle: {
+            color: new echarts.graphic.LinearGradient(1, 0, 0, 0, [
+              { offset: 0, color: '#ffb1d8' },
+              { offset: 0.5, color: '#ff7fbd' },
+              { offset: 1, color: '#ffb1d8' },
+            ]),
+          },
+          emphasis: {
+            itemStyle: {
+              color: new echarts.graphic.LinearGradient(1, 0, 0, 0, [
+                { offset: 0, color: '#ffb1d8' },
+                { offset: 0.7, color: '#ff4da2' },
+                { offset: 1, color: '#fe1e87' },
+              ]),
+            },
+          },
+        },
+      ],
+      animationDuration: 0,
+      animationDurationUpdate: 700,
+      animationEasing: 'quarticInOut',
+      animationEasingUpdate: 'quarticInOut',
+    };
+  }, [fieldDistributionDetailChartIndex, stats?.indexes]);
 
   const indexList = useMemo(() => {
     if (indexes && indexes.length > 0) {
@@ -39,11 +152,25 @@ function IndexesLayout() {
             }}
           >
             <p className={`col-span-4 text-xl font-bold`}>{uid}</p>
-            <div className={`col-span-4 flex justify-between items-center`}>
-              <Badge size="lg" variant="outline">
+            <div className={`col-span-4 flex justify-end gap-x-2 items-center`}>
+              <Badge className={`mr-auto`} size="lg" variant="outline">
                 Count: {indexStat?.numberOfDocuments ?? 0}
               </Badge>
 
+              <Tooltip label="Field Distribution">
+                <ActionIcon
+                  variant="light"
+                  color={'brand'}
+                  onClick={
+                    ((e) => {
+                      e.stopPropagation();
+                      onClickFieldDistribution(index);
+                    }) as MouseEventHandler<HTMLButtonElement>
+                  }
+                >
+                  <IconAbacus size={24} />
+                </ActionIcon>
+              </Tooltip>
               <Tooltip label="Settings">
                 <ActionIcon
                   variant="light"
@@ -72,7 +199,7 @@ function IndexesLayout() {
         </div>
       );
     }
-  }, [indexes, onClickIndex, searchParams, stats?.indexes]);
+  }, [indexes, onClickFieldDistribution, onClickIndex, searchParams, stats?.indexes]);
 
   return useMemo(
     () => (
@@ -102,9 +229,22 @@ function IndexesLayout() {
             <Outlet context={{ refreshIndexes: () => indexesQuery.refetch() }} />
           </div>
         </div>
+        <Modal
+          opened={isFieldDistributionDetailModalOpen}
+          onClose={() => setIsFieldDistributionDetailModalOpen(false)}
+          centered
+          lockScroll
+          radius="lg"
+          shadow="xl"
+          overlayOpacity={0.3}
+          padding="xl"
+          withCloseButton={false}
+        >
+          <ReactECharts className={``} option={fieldDistributionChartOpt} notMerge={true} lazyUpdate={true} />
+        </Modal>
       </div>
     ),
-    [client, indexList, indexesQuery]
+    [client, fieldDistributionChartOpt, indexList, indexesQuery, isFieldDistributionDetailModalOpen]
   );
 }
 
