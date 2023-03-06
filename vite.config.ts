496a68858bbca279e151df972123969c4ab0a299
Ricco Xie <ricco@riccox.com>
2023-03-06 13:48:58 +0800

    perf: build chunk split more fine-grained.
    consider pnpm node_modules special link to split chunks.

--- vite.config.ts
+++ vite.config.ts
@@ -20,9 +20,11 @@ export default defineConfig({
         manualChunks(id) {
           // node_modules is mostly the main reason for the large chunk problem,
           // With this you're telling Vite to treat the used modules separately.
-          // To understand better what it does, try to compare the logs from the build command with and without this change.
+          // To understand better what it does,
+          // try to compare the logs from the build command with and without this change.
           if (id.includes('node_modules')) {
-            return id.toString().split('node_modules/')[1].split('/')[0].toString();
+            const importStrArr = id.toString().split('node_modules/');
+            return importStrArr[importStrArr.length - 1].split('/')[0].toString();
           }
         },
       },
