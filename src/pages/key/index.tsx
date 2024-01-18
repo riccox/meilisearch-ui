d971f531868b0c1dca18816057e16c06a6b9b470
Ricco Xie <ricco@riccox.com>
2024-01-18 20:40:54 +0800

    fix: gh-107 rename stroke-width to strokeWidth.

--- src/pages/key/index.tsx
+++ src/pages/key/index.tsx
@@ -378,7 +378,7 @@ function Keys() {
                       width={24}
                       height={24}
                       viewBox="0 0 24 24"
-                      stroke-width={2}
+                      strokeWidth={2}
                       stroke="currentColor"
                       fill="none"
                       strokeLinecap="round"
