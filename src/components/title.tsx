e1d7a2402dac6ccf01d51eef0bf4d57f487d417f
Ricco Xie <ricco@riccox.com>
2024-06-25 21:00:12 +0800

    feat: build fieldDistribution page.
    feat: rebuild index page layout.

--- src/components/title.tsx
+++ src/components/title.tsx
@@ -1,4 +1,5 @@
 'use client';
+import { cn } from '@/lib/cn';
 import { FC } from 'react';
 interface Props {
   className?: string;
@@ -7,7 +8,7 @@ interface Props {
 
 export const TitleWithUnderline: FC<Props> = ({ className = '', children }) => {
   return (
-    <div className="w-fit text-3xl font-bold relative z-10">
+    <div className={cn(className, 'w-fit text-3xl font-bold relative z-10 text-ellipsis')}>
       {children}
       <div className="-z-10 absolute w-full h-[0.8rem] bg-primary-400/75 -bottom-[2px] rounded-lg"></div>
     </div>
--- src/routes/ins/$insID/_layout/index/$indexUID/fieldDistribution.tsx
+++
@@ -1,5 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router';
-
-export const Route = createFileRoute('/ins/$insID/_layout/index/$indexUID/fieldDistribution')({
-  component: () => <div>Hello /ins/$insID/_layout/index/$indexUID/fieldDistribution!</div>,
-});
