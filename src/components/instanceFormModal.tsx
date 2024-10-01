844bfb5d674bd52b3e56b2c92e03f804f2d8e2f6
Ricco Xie <ricco@riccox.com>
2024-10-01 11:46:19 +0800

    fix: ins form validation logic.

--- src/components/instanceFormModal.tsx
+++ src/components/instanceFormModal.tsx
@@ -4,10 +4,10 @@ import { Drawer } from 'vaul';
 import { cn } from '@/lib/cn';
 import { Footer } from './Footer';
 import { useTranslation } from 'react-i18next';
-import { useForm } from 'react-hook-form';
+import { Controller, useForm } from 'react-hook-form';
 import { defaultInstance, Instance, useAppStore } from '@/store';
 import { useCurrentInstance } from '@/hooks/useCurrentInstance';
-import { Autocomplete, Input, Button, Tooltip, AutocompleteItem } from '@nextui-org/react';
+import { Input, Button, Tooltip } from '@nextui-org/react';
 import _ from 'lodash';
 import { testConnection } from '@/utils/conn';
 
@@ -89,10 +89,10 @@ export const InsFormModal: FC<Props> = ({ className = '', children, type = 'crea
           >
             <div bg-zinc-4 rounded-2xl w-15 h-2 self-center></div>
             <p className={`text-center font-semibold text-lg`}>{t(`form.title.${instanceFormType}`)}</p>
-            <Input
-              label={t('form.name.label')}
-              placeholder={t('form.name.placeholder')}
-              {...form.register('name', {
+            <Controller
+              control={form.control}
+              name="name"
+              rules={{
                 validate: (value: string) => {
                   let otherNames: string[] = [];
                   switch (instanceFormType) {
@@ -110,43 +110,60 @@ export const InsFormModal: FC<Props> = ({ className = '', children, type = 'crea
                     return true;
                   }
                 },
-              })}
+              }}
+              render={({ field, fieldState }) => (
+                <Input
+                  label={t('form.name.label')}
+                  placeholder={t('form.name.placeholder')}
+                  isInvalid={!!fieldState.error?.message}
+                  errorMessage={fieldState.error?.message}
+                  {...field}
+                />
+              )}
             />
-            <Tooltip placement={'bottom-start'} content={t('form.host.tip')}>
-              <Autocomplete
-                label={t('form.host.label')}
-                placeholder="http://127.0.0.1:7700"
-                isClearable={false}
-                {...form.register('host', {
-                  validate: (value) => {
-                    if (
-                      /(https?:\/\/)?(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()!@:%_+.~#?&//=]*)/.test(
-                        value
-                      )
-                    ) {
-                      return true;
-                    } else {
-                      form.setError('host', { message: 'Host not valid' });
-                      return false;
-                    }
-                  },
-                })}
-              >
-                {instances.map((i) => (
-                  <AutocompleteItem key={i.host} value={i.host}>
-                    {i.host}
-                  </AutocompleteItem>
-                ))}
-              </Autocomplete>
-            </Tooltip>
-            <Tooltip placement={'bottom-start'} content={t('form.api_key.tip')}>
-              <Input
-                type="password"
-                placeholder="masterKey"
-                label={t('form.api_key.label')}
-                {...form.register('apiKey')}
-              />
-            </Tooltip>
+            <Controller
+              control={form.control}
+              name="host"
+              rules={{
+                validate: (value) => {
+                  if (/^(https?:\/\/)?([a-zA-Z0-9-]+((\.[a-zA-Z0-9-]+)+)?(:\d+)?)(\/[^\s]*)?$/.test(value)) {
+                    return true;
+                  } else {
+                    form.setError('host', { message: 'Host not valid' });
+                    return false;
+                  }
+                },
+              }}
+              render={({ field, fieldState }) => (
+                <>
+                  <Tooltip placement={'bottom-start'} content={t('form.host.tip')}>
+                    <Input
+                      placeholder="http://127.0.0.1:7700"
+                      label={t('form.host.label')}
+                      isInvalid={!!fieldState.error?.message}
+                      errorMessage={fieldState.error?.message}
+                      {...field}
+                    />
+                  </Tooltip>
+                </>
+              )}
+            />
+            <Controller
+              control={form.control}
+              name="apiKey"
+              render={({ field, fieldState }) => (
+                <Tooltip placement={'bottom-start'} content={t('form.api_key.tip')}>
+                  <Input
+                    type="password"
+                    placeholder="masterKey"
+                    label={t('form.api_key.label')}
+                    isInvalid={!!fieldState.error?.message}
+                    errorMessage={fieldState.error?.message}
+                    {...field}
+                  />
+                </Tooltip>
+              )}
+            />
             <Button type="submit" variant="solid" size="sm" color="primary" disabled={isSubmitInstanceLoading}>
               {t('confirm')}
             </Button>
