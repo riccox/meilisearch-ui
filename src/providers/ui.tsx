e06aa0df86f3562420cd7173f4ebf2802a1d0bee
Ricco Xie <ricco@riccox.com>
2024-10-08 10:12:17 +0800

    perf: add Semi components i18n support.

--- src/providers/ui.tsx
+++ src/providers/ui.tsx
@@ -5,36 +5,42 @@ import _ from 'lodash';
 import { ModalsProvider } from '@mantine/modals';
 import '@mantine/core/styles.css';
 import { NextUIProvider } from '@nextui-org/react';
+import { LocaleProvider } from '@douyinfe/semi-ui';
+import { lang2SemiLocale, SUPPORTED_LANGUAGE } from '@/utils/i18n';
+import { useTranslation } from 'react-i18next';
 
 type Props = {
   children: ReactNode;
 };
 
 export const UIProvider: FC<Props> = ({ children }) => {
+  const { i18n } = useTranslation();
   return (
-    <MantineProvider
-      theme={{
-        colors: _.transform(_.pick(theme.colors, ['primary', 'secondary']), (result, value, key) => {
-          result || (result = {});
-          result[key] = Object.values(value) as unknown as MantineColorsTuple;
-        }),
-        primaryColor: 'primary',
-      }}
-    >
-      <ModalsProvider
-        modalProps={{
-          centered: true,
-          lockScroll: true,
-          shadow: 'xl',
-          radius: 'lg',
-          padding: 'xl',
-          overlayProps: {
-            opacity: 0.3,
-          },
+    <LocaleProvider locale={lang2SemiLocale(i18n.resolvedLanguage as SUPPORTED_LANGUAGE)}>
+      <MantineProvider
+        theme={{
+          colors: _.transform(_.pick(theme.colors, ['primary', 'secondary']), (result, value, key) => {
+            result || (result = {});
+            result[key] = Object.values(value) as unknown as MantineColorsTuple;
+          }),
+          primaryColor: 'primary',
         }}
       >
-        <NextUIProvider>{children}</NextUIProvider>
-      </ModalsProvider>
-    </MantineProvider>
+        <ModalsProvider
+          modalProps={{
+            centered: true,
+            lockScroll: true,
+            shadow: 'xl',
+            radius: 'lg',
+            padding: 'xl',
+            overlayProps: {
+              opacity: 0.3,
+            },
+          }}
+        >
+          <NextUIProvider>{children}</NextUIProvider>
+        </ModalsProvider>
+      </MantineProvider>
+    </LocaleProvider>
   );
 };
