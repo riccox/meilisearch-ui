51bd689fb9345a0f36e6435f4ffce94631ad3358
Ricco Xie <ricco@riccox.com>
2024-10-10 10:51:45 +0800

    feat: add search result download btn.

---
+++ src/utils/file.ts
@@ -0,0 +1,26 @@
+'use client';
+
+export const downloadFile = ({ data, fileName, fileType }: { data: BlobPart; fileName: string; fileType: string }) => {
+  // Create a blob with the data we want to download as a file
+  const blob = new Blob([data], { type: fileType });
+  // Create an anchor element and dispatch a click event on it
+  // to trigger a download
+  const a = document.createElement('a');
+  a.download = fileName;
+  a.href = window.URL.createObjectURL(blob);
+  const clickEvt = new MouseEvent('click', {
+    view: window,
+    bubbles: true,
+    cancelable: true,
+  });
+  a.dispatchEvent(clickEvt);
+  a.remove();
+};
+
+export const exportToJSON = (obj: object | object[], fileName: string) => {
+  downloadFile({
+    data: JSON.stringify(obj),
+    fileName: `${fileName}.json`,
+    fileType: 'text/json',
+  });
+};
