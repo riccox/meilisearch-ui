5d68227d678c2b55f4cb86d054edb7b7256cefcc
Karan Janthe <86996507+KMJ-007@users.noreply.github.com>
2024-10-23 15:34:53 +0530

    fix:not loading tanstack router dev tools in production

--- src/routes/__root.tsx
+++ src/routes/__root.tsx
@@ -1,5 +1,14 @@
 import { createRootRoute, Outlet } from '@tanstack/react-router';
-import { TanStackRouterDevtools } from '@tanstack/router-devtools';
+
+const TanStackRouterDevtools =
+  process.env.NODE_ENV === 'production'
+    ? () => null // Render nothing in production
+    : React.lazy(() =>
+        // Lazy load in development
+        import('@tanstack/router-devtools').then((res) => ({
+          default: res.TanStackRouterDevtools
+        })),
+      )
 
 export const Route = createRootRoute({
   component: () => (
