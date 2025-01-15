c8700bdbfe0b210bfd2e3eb6acf8622e57f067c7
Ricco Xie <ricco@riccox.com>
2025-01-15 23:00:15 +0800

    fix: resource load error cause by relative path. gh-185

--- vite.config.ts
+++ vite.config.ts
@@ -27,7 +27,7 @@ export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, process.cwd(), '');
   console.debug('print current base path', env.BASE_PATH);
   return {
-    base: env.BASE_PATH || '',
+    base: env.BASE_PATH || '/',
     plugins: [
       tsconfigPaths({ root: './' }),
       react(),
@@ -71,11 +71,6 @@ export default defineConfig(({ mode }) => {
             }
           },
         },
-      },
-    },
-    experimental: {
-      renderBuiltUrl() {
-          return { relative: true };
       },
     },
   };
